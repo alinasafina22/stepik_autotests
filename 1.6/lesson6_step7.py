6324f425a856308db56df66f687e6c97c08bdd33
alinasafina22 <alinasafina22@gmail.com>
2023-03-05 19:55:53 +0700

    Добавила папку 1.6

---
+++ 1.6/lesson6_step7.py
@@ -0,0 +1,35 @@
+"""
+Задание: поиск элемента по XPath
+На этот раз воспользуемся возможностью искать элементы по XPath.
+
+На странице http://suninjuly.github.io/find_xpath_form вы найдете такую же форму регистрации,
+как в шаге 3, при этом в нее добавилась куча одинаковых кнопок отправки. Но сработает только кнопка с текстом "Submit",
+и наша задача нажать в коде именно на неё.
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://suninjuly.github.io/find_xpath_form"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.NAME, 'first_name')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, 'last_name')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CSS_SELECTOR, ".city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.XPATH, "//button[text()='Submit']")
+    button.click()
+
+finally:
+
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
